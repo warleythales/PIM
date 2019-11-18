7d407c34ac4adbc6e91e9c1f2286930ad1e40c16
Warley Lopes <wtsl_@hotmail.com>
2019-11-18 00:32:17 -0300

    Model Area de Lazer

--- Model/AreaLazer.cs
+++ Model/AreaLazer.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    class AreaLazer
+    class AreaLazer : Basico
     {
+        public string Descricao { get; set; }
+        public int CapacidadePessoas { get; set; }
+        public bool Visitante { get; set; }
+        public bool Aluguel { get; set; }
+        public string Status { get; set; }
+        public int IdTorre { get; set; }
     }
 }
