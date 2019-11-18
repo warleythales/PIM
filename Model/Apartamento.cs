f39433aa02d6db6f60154d30bec8fb57c9928e69
Warley Lopes <wtsl_@hotmail.com>
2019-11-18 00:17:09 -0300

    Add project files.

---
+++ Model/Apartamento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Apartamento : Basico
+    {
+        public int Numero { get; set; }
+        public int QtdQuartos { get; set; }
+        public int QtdMoradores { get; set; }
+        public bool Disponivel { get; set; }
+        public string Tipo { get; set; }
+        public int IdTorre { get; set; }
+    }
+}
