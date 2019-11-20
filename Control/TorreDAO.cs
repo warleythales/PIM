9b9e323e2c95d60d72e28c1b1cc6806949de5369
Cleyton Urani <cleytonnurani@gmail.com>
2019-11-20 13:39:33 -0300

    ajuste dao visitente e add dao torre

---
+++ Control/TorreDAO.cs
@@ -0,0 +1,142 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control
+{
+    public class TorreDAO
+    {
+        private static string strConection = ConfigurationManager.AppSettings["connection"].ToString();
+
+        public static List<Model.Torre> Listar()
+        {
+            using (MySqlConnection conn = new MySqlConnection(strConection))
+            {
+                conn.Open();
+
+                using (MySqlCommand cmd = new MySqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = @"SELECT * FROM torre";
+
+                    using (MySqlDataAdapter da = new MySqlDataAdapter())
+                    {
+                        da.SelectCommand = cmd;
+
+                        DataSet ds = new DataSet();
+                        da.Fill(ds, "Torre");
+
+                        List<Model.Torre> lstRetorno = ds.Tables["Torre"].AsEnumerable().Select(x => new Model.Torre
+                        {
+                            Id = x.Field<int>("id"),
+                            Descricao = x.Field<string>("descricao"),
+                            Academia = x.Field<bool>("academia"),
+                            CondominioId = x.Field<int>("idCondominio"),
+                            Pavimentos = x.Field<int>("pavimentos"),
+                            SalaoFesta = x.Field<bool>("SalaoDeFesta"),
+                            Vagas = x.Field<int>("vagas"),
+                            Status = x.Field<bool>("status")
+                        }).ToList();
+
+                        return lstRetorno;
+                    }
+                }
+            }
+        }
+
+        public static void Salvar(Model.Torre torre)
+        {
+            using (MySqlConnection conn = new MySqlConnection(strConection))
+            {
+                conn.Open();
+
+                using (MySqlCommand cmd = new MySqlCommand())
+                {
+                    cmd.Connection = conn;
+
+                    if (torre.Id == 0)
+                        cmd.CommandText = @"INSERT INTO torre
+                                                (descricao, pavimentos, vagas, academia, SalaoDeFesta, status, idCondominio)
+                                            VALUES
+                                                (?descricao, ?pavimentos, ?vagas, ?academia, ?SalaoDeFesta, ?status, ?idCondominio);";
+                    else
+                        cmd.CommandText = @"UPDATE torre 
+                                                SET descricao = ?descricao,
+                                                    pavimentos = ?pavimentos,
+                                                    vagas = ?vagas,
+                                                    academia = ?academia,
+                                                    SalaoDeFesta = ?SalaoDeFesta,
+                                                    status = ?status,
+                                                    idCondominio = ?idCondominio
+                                            WHERE id = ?id;";
+
+                    cmd.Parameters.AddWithValue("?descricao", torre.Descricao);
+                    cmd.Parameters.AddWithValue("?pavimentos", torre.Pavimentos);
+                    cmd.Parameters.AddWithValue("?vagas", torre.Vagas);
+                    cmd.Parameters.AddWithValue("?academia", torre.Academia);
+                    cmd.Parameters.AddWithValue("?SalaoDeFesta", torre.SalaoFesta);
+                    cmd.Parameters.AddWithValue("?status", torre.Status);
+                    cmd.Parameters.AddWithValue("?idCondominio", torre.CondominioId);
+                    cmd.Parameters.AddWithValue("?id", torre.Id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static void Excluir(int id)
+        {
+            using (MySqlConnection conn = new MySqlConnection(strConection))
+            {
+                conn.Open();
+
+                using (MySqlCommand cmd = new MySqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = @"DELETE FROM torre WHERE id = ?id";
+                    cmd.Parameters.AddWithValue("?id", id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static Model.Torre BuscarPorId(int clienteId)
+        {
+            using (MySqlConnection conn = new MySqlConnection(strConection))
+            {
+                conn.Open();
+
+                using (MySqlCommand cmd = new MySqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = @"SELECT * FROM torre WHERE id = ?id";
+                    cmd.Parameters.AddWithValue("?id", clienteId);
+
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+                    Model.Torre retorno = new Model.Torre();
+
+                    while (reader.Read())
+                    {
+                        retorno.Id = (int)reader["Id"];
+                        retorno.Descricao = (string)reader["descricao"];
+                        retorno.Academia = (bool)reader["academia"];
+                        retorno.CondominioId = (int)reader["idCondominio"];
+                        retorno.Pavimentos = (int)reader["pavimentos"];
+                        retorno.SalaoFesta = (bool)reader["SalaoDeFesta"];
+                        retorno.Vagas = (int)reader["vagas"];
+                        retorno.Status = (bool)reader["status"];
+                    }
+
+                    return retorno;
+                }
+            }
+        }
+    }
+}
---
+++ Control/VisitanteDAO.cs
@@ -0,0 +1,131 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control
+{
+    public class VisitanteDAO
+    {
+        private static string strConection = ConfigurationManager.AppSettings["connection"].ToString();
+
+        public static List<Model.Visitante> Listar()
+        {
+            using (MySqlConnection conn = new MySqlConnection(strConection))
+            {
+                conn.Open();
+
+                using (MySqlCommand cmd = new MySqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = @"SELECT * FROM visitante";
+
+                    using (MySqlDataAdapter da = new MySqlDataAdapter())
+                    {
+                        da.SelectCommand = cmd;
+
+                        DataSet ds = new DataSet();
+                        da.Fill(ds, "Visitante");
+
+                        List<Model.Visitante> lstRetorno = ds.Tables["Visitante"].AsEnumerable().Select(x => new Model.Visitante
+                        {
+                            Id = x.Field<int>("id"),
+                            CPF = x.Field<string>("cpf"),
+                            Email = x.Field<string>("email"),
+                            Nome = x.Field<string>("nome"),
+                            RG = x.Field<string>("rg"),
+                            Telefone = x.Field<string>("telefone")
+                        }).ToList();
+
+                        return lstRetorno;
+                    }
+                }
+            }
+        }
+
+        public static void Salvar(Model.Visitante visitante)
+        {
+            using (MySqlConnection conn = new MySqlConnection(strConection))
+            {
+                conn.Open();
+
+                using (MySqlCommand cmd = new MySqlCommand())
+                {
+                    cmd.Connection = conn;
+
+                    if (visitante.Id == 0)
+                        cmd.CommandText = @"INSERT INTO visitante (cpf, email, nome, rg, telefone) VALUES (?cpf, ?email, ?nome, ?rg, ?telefone);";
+                    else
+                        cmd.CommandText = @"UPDATE visitante 
+                                                SET cpf = ?cpf,
+                                                    email = ?email,
+                                                    nome = ?nome,
+                                                    rg = ?rg,
+                                                    telefone = ?telefone
+                                            WHERE id = ?id;";
+
+                    cmd.Parameters.AddWithValue("?cpf", visitante.CPF);
+                    cmd.Parameters.AddWithValue("?email", visitante.Email);
+                    cmd.Parameters.AddWithValue("?nome", visitante.Nome);
+                    cmd.Parameters.AddWithValue("?rg", visitante.RG);
+                    cmd.Parameters.AddWithValue("?telefone", visitante.Telefone);
+                    cmd.Parameters.AddWithValue("?id", visitante.Id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static void Excluir(int id)
+        {
+            using (MySqlConnection conn = new MySqlConnection(strConection))
+            {
+                conn.Open();
+
+                using (MySqlCommand cmd = new MySqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = @"DELETE FROM visitante WHERE id = ?id";
+                    cmd.Parameters.AddWithValue("?id", id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static Model.Visitante BuscarPorId(int clienteId)
+        {
+            using (MySqlConnection conn = new MySqlConnection(strConection))
+            {
+                conn.Open();
+
+                using (MySqlCommand cmd = new MySqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = @"SELECT * FROM visitante WHERE id = ?id";
+                    cmd.Parameters.AddWithValue("?id", clienteId);
+
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+                    Model.Visitante retorno = new Model.Visitante();
+
+                    while (reader.Read())
+                    {
+                        retorno.Id = (int)reader["Id"];
+                        retorno.CPF = (string)reader["cpf"];
+                        retorno.Email = (string)reader["email"];
+                        retorno.Nome = (string)reader["nome"];
+                        retorno.RG = (string)reader["rg"];
+                        retorno.Telefone = (string)reader["telefone"];
+                    }
+
+                    return retorno;
+                }
+            }
+        }
+    }
+}
