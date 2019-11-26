79ab9e6ae16a958b51b4d6f8c42aa48bffd549e4
Warley Lopes <wtsl_@hotmail.com>
2019-11-26 00:30:58 -0300

    Add AreaLazerController

---
+++ Views/Controllers/AreaLazerController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Views.Controllers
+{
+    public class AreaLazerController : Controller
+    {
+        // GET: AreaLazer
+        public ActionResult Index()
+        {
+            return View(Control.AreaLazerDAO.Listar());
+        }
+
+        // GET: AreaLazer/Create
+        public ActionResult Create()
+        {
+            ViewBag.Torre = Control.ItensSelecao.Torre.GetSelectListItems;
+            return View();
+        }
+
+        // POST: AreaLazer/Create
+        [HttpPost]
+        public ActionResult Create(Model.AreaLazer arealazer)
+        {
+            try
+            {
+                Control.AreaLazerDAO.Salvar(arealazer);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AreaLazer/Edit/5
+        public ActionResult Edit(int id)
+        {
+            ViewBag.Torre = Control.ItensSelecao.Torre.GetSelectListItems;
+            return View(Control.AreaLazerDAO.BuscarPorId(id));
+        }
+
+        // POST: AreaLazer/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Model.AreaLazer arealazer)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                Control.AreaLazerDAO.Salvar(arealazer);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Condomino/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(Control.AreaLazerDAO.BuscarPorId(id));
+        }
+
+        // POST: Condomino/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection form)
+        {
+            try
+            {
+                Control.AreaLazerDAO.Excluir(id);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
